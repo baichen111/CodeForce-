0a1009fbd1f041ff2fe13e6cb88b657bb9a287cb
Bai chen <35583877+baichen111@users.noreply.github.com>
2023-10-28 23:54:50 +0800

    Add files via upload

---
+++ DivisibilityProblem.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define int long long int
+//#define ONLINE_JUDGE
+
+using namespace std;
+
+int32_t main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin >> t;
+	int x, y;
+	while (t--) {
+		cin >> x >> y;
+		int counter = 0;
+		if (x <= y) {
+			cout << (y - x) << endl;
+			continue;
+		}
+
+		if (x % y == 0 ) {
+			cout << counter << endl;
+			continue;
+		};
+
+		int res = x % y;
+		cout << y - res << endl;
+
+	}
+	return 0;
+}
