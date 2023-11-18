8ddf61a0df696238f62ea73b4431a43df052d049
Bai chen <35583877+baichen111@users.noreply.github.com>
2023-11-18 19:56:57 +0800

    Add files via upload

---
+++ AntonandPolyhedrons.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define int long long int
+//#define ONLINE_JUDGE
+
+using namespace std;
+
+int32_t main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	map<string, int> m {{"Tetrahedron", 4}, {"Cube", 6}, {"Octahedron", 8}, {"Dodecahedron", 12}, {"Icosahedron", 20}};
+	int n;
+	cin >> n;
+	string s;
+	int counter = 0;
+	while (cin >> s) {
+		counter += m[s];
+	}
+	cout << counter;
+
+	return 0;
+}
